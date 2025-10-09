926aac5bf83acf156121b606170b93b46e12d506
vaishnavi16-hub <your_email@example.com>
2025-10-09 11:53:54 +0530

    Added the dsa with strivers folder

---
+++ Dsa_with_strivers/basics.py
@@ -0,0 +1,28 @@
+# Complete the function printNumber which takes an integer input from the user and prints it on the screen.
+
+
+
+# Use:-
+
+# for C++ : cout << variable_name;
+# for Java : System.out.print();
+# for Python : print()
+# for Javascript : console.lo
+
+
+
+
+# SOLUTION:-
+
+# FOR C++...
+
+#include <iostream>
+# using namespace std;
+
+# int main() {
+#     std ::cout << "Hello Vaishnavi!";
+#     return 0;
+# }
+
+# O/P:-
+# 7
