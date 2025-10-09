f0f887967da177cbe6e89666272ed2126108a5a8
vaishnavi16-hub <your_email@example.com>
2025-10-09 14:13:02 +0530

    datatypes

---
+++ datatypes.py
@@ -0,0 +1,90 @@
+# // # DATATYPES...
+
+# // # integer
+
+# // #include <iostream>
+# // using namespace std;
+
+# // int main(){
+# //     int age = 21;
+# //     cout <<"age:" <<age << end1;
+# //     return 0;
+
+# // }
+
+# // output:-
+# // Age: 21
+
+# // FLOAT....
+
+# // #include <iostream>
+# // using namespace std;
+
+# // int main(){
+# //     float height = 5.6;
+# //     cout <<"height:"<<end1;
+# //     return 0;
+# // }
+
+# // height = 5.6
+
+
+# // DOUBLE....
+
+# // #include <iostream>
+
+# // using namespace std;
+
+# // int main(){
+# //     double weight = 55.55;
+# //     cout << weight: "<<weight << end1;"
+# //     return 0;
+# // }
+# // output:- 55.55
+
+
+
+# // CHAR...
+
+
+# // #include <iostream>
+# // using namespace std;
+
+# // int main (){
+# //     char grade = 'A';
+# //     cout <<"Grade:" <<grade << end1;
+# //     return 0;
+# // }
+
+# // output :- 
+# // grade :-A
+
+
+
+# // STRING..
+
+# // #include <iostream>
+# // using namespace std;
+
+# // int main (){
+# //     string name = "vaishnavi ";
+# //     cout << "Name:" "<<name <<end1";
+# //     return 0;
+# // }
+
+# // output:- vaishnavi
+
+
+
+# // BOOLEAN....
+
+# // #include <iostream>
+# // using namespace std;
+
+# // int main() {
+# //     bool isStudent = true;
+# //     cout << "is student : <<is student <<end1;"
+# //     return 0;
+# // }
+
+# // output:- true or false
