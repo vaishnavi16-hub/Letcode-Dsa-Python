e753fdd896dc10516ba51c392718d9a20d111bc5
Vaishnavi Gaikwad <vaishnavi200316@gmail.com>
2025-11-28 13:42:21 +0530

    for loop

---
+++ for-loop.py
@@ -0,0 +1,47 @@
+
+# c++ program..
+
+# include <iostream>
+# # using namespace std;
+
+# logic...
+# i=1
+# i=2
+# i=3
+
+# int main() {
+#     for(int i = 1; i <= 10; i = i + 1) {
+#         cout << "vaishnavi" << endl;
+#     }
+#     return 0;
+# }
+
+
+
+#  vaisnavi
+#  vaisnavi
+#  vaisnavi
+#  vaisnavi
+#  vaisnavi
+#  vaisnavi
+#  vaisnavi
+#  vaisnavi
+#  vaisnavi
+
+
+# PYTHON ...
+
+for i in range(1,11):
+    print("vaishnavi")
+
+
+output :- vaishnavi
+vaishnavi
+vaishnavi
+vaishnavi
+vaishnavi
+vaishnavi
+vaishnavi
+vaishnavi
+vaishnavi
+vaishnavi
